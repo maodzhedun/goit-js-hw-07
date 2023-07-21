cca8ba8aee227b0af439f15ab18b0da0fea22134
maodzhedun <maodzhedun@gmail.com>
2023-07-21 11:51:25 +0300

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
-// console.dir(galleryContainer);
 
 const galleryMarkup = createGalleryHTMLMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryContainer.addEventListener()
+galleryContainer.addEventListener('click', onClick);
 
 function createGalleryHTMLMarkup(galleryItems) { 
   return galleryItems.map(
-    ({ preview, fullimage,description }) => `
+    ({ preview, original, description }) => `
   <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      data-source="${fullimage}"
+      data-source="${original}"
       alt="${description}"
     />
   </a>
@@ -28,3 +27,31 @@ function createGalleryHTMLMarkup(galleryItems) {
     .join("");
 }
 
+function onClick(event) {
+  event.preventDefault();
+  // console.dir(event.target);
+  if (event.target.tagName !== "IMG") {
+    return;
+  }
+  // console.log(event.currentTarget);
+  console.log(event.target.dataset.source);
+}
+
+function openModal() {
+  
+}
+
+
+// function onKey(evt) {
+//   console.log(evt);
+//   // if(evt.code === 'Escape' || evt.code === 'Enter'){
+//   //     container.classList.toggle('tog')
+//   // }
+
+//   if (evt.ctrlKey && (evt.code === "KeyC")) {
+//     console.log('try copy');
+//     evt.preventDefault()
+//     // alert("Копіюєш?");
+//     return;
+//   }
+// }
